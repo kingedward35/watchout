a3a5c76da76792a9f494354e48bc5de70c8c0517
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-08-29 17:04:55 -0600

    Finished Basic watchout

--- watchout.js
+++ watchout.js
@@ -1,63 +1,98 @@
 var width = 700, height = 450;
 
-var initialXAstroidPositions = [];
-var initialYAstroidPositions = [];
+var n = 10;
+var enemyRadius = d3.range(5, 5*(n + 1), 5);
+var ssRadius = [50];
+var time = 1000;
+
 var initialXUserPosition = Math.floor(width/2);
 var initialYUserPosition = Math.floor(height/2);
-var n = 10;
+
 var currentScore = 0;
 var highestScore = 0;
 var collisions = 0;
 
-for (var i = 0; i < 10; i++) {
-  var tempX = Math.random() * width;
-  initialXAstroidPositions.push(tempX);
-  tempX = Math.random() * height;
-  initialYAstroidPositions.push(tempX);
+var updateScore = function() {
+  d3.select('.scoreboard .current span').text(currentScore);
+  d3.select('.scoreboard .high span').text(highestScore);
+  d3.select('.scoreboard .collisions span').text(collisions);
 }
+
+
+
 
 var svg = d3.select("body").append("svg")
 .attr("width", width).attr("height", height)
 .append("g");
 
-function Initialize() {
-  var ast = svg.selectAll('image');
+  var ast = svg.selectAll('circle').data(enemyRadius);
 
-  ast.enter().append('image').
-  .attr('x', function(d, i) {
-    return initialXAstroidPositions[i];
-  })
-  .attr('y', function(d, i) {
-    return initialYAstroidPositions[i];
-  })
-  .attr('xlink:href', function(d, i) {
-    return "asteroid.png";
+  ast.enter().append('circle')
+  .attr('class',function(d,i) {return 'astroid';})
+  .attr('r', function(d,i) {return d;})
+  .attr('cx',function(d,i) {return Math.random() * width;})
+  .attr('cy', function(d,i) {return Math.random() * height});
+
+  var ss = svg.selectAll('circle.spaceship').data(ssRadius);
+  ss.enter().append('circle')
+  .attr('class','spaceship')
+  .attr('r', function(d,i) {return d;})
+  .attr('cx',initialXUserPosition)
+  .attr('cy',initialYUserPosition);
+
+
+var drag = d3.behavior.drag();
+  drag.on('drag', function () {
+    ss.attr('cx', d3.event.x)
+      .attr('cy', d3.event.y)
   });
+  ss.call(drag);
+
+var move = function(element) {
+  element.transition().duration(time)
+  .attr('cx',function(d,i) {return Math.random() * width;})
+  .attr('cy', function(d,i) {return Math.random() * height;})
+  .each('end', function() {
+    move(d3.select(this));
+  });
+};
+move(ast);
 
 
-}
+var scoreTicker = function() {
+  currentScore++;
+  highestScore = Math.max(highestScore,currentScore);
+  updateScore();
+};
 
-function update() {
-  var ast = svg.selectAll('image');
 
-  if (ast.attr('xlink:href') !== "asteroid.png") {
 
+var prevCollision = false;
+
+var collisionDetect = function () {
+
+  var col = false;
+
+  ast.each(function() {
+    var cx = parseInt(this.getAttribute('cx'));
+    var cy = parseInt(this.getAttribute('cy'));
+    var x = parseInt(ss.attr('cx')) - cx;
+    var y = parseInt(ss.attr('cy')) - cy;
+    if (Math.sqrt(x*x + y*y) < parseFloat(this.getAttribute('r')) + ssRadius[0]) {
+      col = true;
+    }
+  });
+  // console.log(col);
+
+  if (col) {
+    currentScore = 0;
+    if (prevCollision !== col) {
+      collisions++;
+    }
   }
-
-  ast.enter().append('image').
-  .attr('x', function(d,i) {
-    return initialXAstroidPositions[i];
-  })
-  .attr('y', function(d, i) {
-    return initialYAstroidPositions[i];
-  });
+  prevCollision = col;
 
 
-}
-
-//Initialize everything
-Initialize();
-
-setInterval(function () {
-  update();
-}, 1000);
+};
+d3.timer(collisionDetect);
+setInterval(scoreTicker, 100);
