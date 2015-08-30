48cf383b5e2cfd7958e08b35a49fd8cb5bf51ed7
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-08-29 19:49:14 -0600

    Watchout

--- watchout.js
+++ watchout.js
@@ -1,8 +1,8 @@
-var width = 700, height = 450;
+var width = 1000, height = 700;
 
 var n = 5;
 var enemyRadius = d3.range(5, 5*(n + 1), 5);
-var ssRadius = [50];
+var ssRadius = [10];
 var time = 1000;
 
 var initialXUserPosition = Math.floor(width/2);
@@ -12,14 +12,19 @@ var currentScore = 0;
 var highestScore = 0;
 var collisions = 0;
 
+function inXbounds(radius, currentX) {
+  return Math.max(radius, Math.min(width - radius, currentX));
+}
+
+function inYbounds(radius, currentY) {
+  return Math.max(radius, Math.min(height - radius, currentY));
+}
+
 var updateScore = function() {
   d3.select('.scoreboard .current span').text(currentScore);
   d3.select('.scoreboard .high span').text(highestScore);
   d3.select('.scoreboard .collisions span').text(collisions);
-}
-
-
-
+};
 
 var svg = d3.select("body").append("svg")
 .attr("width", width).attr("height", height)
@@ -30,8 +35,8 @@ var svg = d3.select("body").append("svg")
   ast.enter().append('circle')
   .attr('class',function(d,i) {return 'astroid';})
   .attr('r', function(d,i) {return d;})
-  .attr('cx',function(d,i) {return Math.random() * width;})
-  .attr('cy', function(d,i) {return Math.random() * height});
+  .attr('cx',function(d,i) {return inXbounds(d,Math.random() * width);})
+  .attr('cy',function(d,i) {return inYbounds(d,Math.random() * height);});
 
   var ss = svg.selectAll('circle.spaceship').data(ssRadius);
   ss.enter().append('circle')
@@ -40,19 +45,19 @@ var svg = d3.select("body").append("svg")
   .attr('cx',initialXUserPosition)
   .attr('cy',initialYUserPosition);
 
-
 var drag = d3.behavior.drag();
   drag.on('drag', function () {
-    ss.attr('cx', d3.event.x)
-      .attr('cy', d3.event.y)
+    ss.attr('cx', inXbounds(ssRadius, d3.event.x))
+      .attr('cy', inYbounds(ssRadius, d3.event.y))
   });
   ss.call(drag);
+   
 
 var move = function(element) {
   element.transition().duration(time)
-  .attr('cx',function(d,i) {return Math.random() * width;})
-  .attr('cy', function(d,i) {return Math.random() * height;})
-  .each('end', function() {
+  .attr('cx',function(d,i) {return inXbounds(d,Math.random() * width);})
+  .attr('cy',function(d,i) {return inYbounds(d,Math.random() * height);})
+  .each('end',function() {
     move(d3.select(this));
   });
 };
@@ -82,17 +87,20 @@ var collisionDetect = function () {
       col = true;
     }
   });
-  // console.log(col);
 
   if (col) {
     currentScore = 0;
     if (prevCollision !== col) {
+      d3.select('body').style({'background-color': "pink"});
       collisions++;
     }
+  }
+  if (!col) {
+  d3.select('body').style({'background-color': "white"});
   }
   prevCollision = col;
 
 
 };
 d3.timer(collisionDetect);
-setInterval(scoreTicker, 100);
+setInterval(scoreTicker, 1);
